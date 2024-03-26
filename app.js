ef16db2f7492c6dd2a1e9ce81b652571e1588476
zaranggi <donnyirianto.anggriawan@gmail.com>
2024-03-26 12:05:05 +0700

    update all

--- app.js
+++ app.js
@@ -1,102 +1,171 @@
-import autoLoad from '@fastify/autoload';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-import Fastify from 'fastify';
-import { v4 as uuidv4 } from 'uuid';
-import compress from '@fastify/compress';
-import cors from '@fastify/cors';
-import redis from '@fastify/redis';
-import metricsPlugin from 'fastify-metrics';
-import fastifyStatic from '@fastify/static';
-import { config } from './config/config.js';
-import {fastifyJWTPlugin} from './pluggins/jwt.js';
-import {fastifyMysql2Plugin} from './pluggins/mysql.js';
-import {fastifyMysql2DmlPlugin} from './pluggins/mysql_dml.js';
-import {fastifyTanggalIndoPlugin} from './pluggins/format_date.js';
-import mqttPlugin from './pluggins/mqttPlugin.js';
+import autoLoad from "@fastify/autoload";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import Fastify from "fastify";
+import { v4 as uuidv4 } from "uuid";
+import compress from "@fastify/compress";
+import cors from "@fastify/cors";
+import redis from "@fastify/redis";
+import metricsPlugin from "fastify-metrics";
+import fastifyStatic from "@fastify/static";
+import { config } from "./config/config.js";
+import { fastifyJWTPlugin } from "./pluggins/jwt.js";
+import { fastifyMysql2Plugin } from "./pluggins/mysql.js";
+import { fastifyMysql2DmlPlugin } from "./pluggins/mysql_dml.js";
+import { fastifyTanggalIndoPlugin } from "./pluggins/format_date.js";
+import mqttPlugin from "./pluggins/mqttPlugin.js";
 //import responseFormatPlugin from './pluggins/response.js';
-import loggerPlugin from './pluggins/logger.js';
-import pino from 'pino';
-import logger from './pluggins/logger.js';
+import loggerPlugin from "./pluggins/logger.js";
+import pino from "pino";
+import logger from "./pluggins/logger.js";
+import * as cron from "node-cron";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+// Load service
+import { getData } from "./services/user/index.js";
+import * as inprogress from "./services/inprogress/index.js";
+import * as urgent from "./services/urgent/index.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const pinoPretty = pino.transport({
-    target: "pino-pretty",
-    options: {
-      translateTime: "HH:MM:ss.l'Z'"
-    }
+  target: "pino-pretty",
+  options: {
+    translateTime: "HH:MM:ss.l'Z'",
+  },
 });
 
 // Combine the streams
 // NOTE: By setting the "level", you can choose what level each individual transport will recieve a log
-const streams = [ 
+const streams = [
   //{level: 'info', stream: pinoLokiTransport},
-  {level: 'info', stream: pinoPretty},
+  { level: "info", stream: pinoPretty },
   //{level: 'warn', stream: pinoLokiTransport},
-  {level: 'warn', stream: pinoPretty},
+  { level: "warn", stream: pinoPretty },
   //{level: 'error', stream: pinoLokiTransport},
-  {level: 'error', stream: pinoPretty},
+  { level: "error", stream: pinoPretty },
   //{level: 'fatal', stream: pinoLokiTransport},
-  {level: 'fatal', stream: pinoPretty}
+  { level: "fatal", stream: pinoPretty },
 ];
- 
-//Config Fastify 
+
+//Config Fastify
 export const fastify = Fastify({
-    // https: {
-    //   key: fs.readFileSync(join(__dirname, '/keys/private.pem')),
-    //   cert: fs.readFileSync(join(__dirname, '/keys/cacert.pem'))
-    // },
-    requestIdLogLabel: 'reqID',
-    genReqId() {
-      return uuidv4();
-    },
-    logger: {
-      stream: pino.multistream(streams),
-      level: 'info'
-    },   
-    disableRequestLogging:true, 
-})
-await fastify.register(compress,{ encodings: ['gzip'] })
+  // https: {
+  //   key: fs.readFileSync(join(__dirname, '/keys/private.pem')),
+  //   cert: fs.readFileSync(join(__dirname, '/keys/cacert.pem'))
+  // },
+  requestIdLogLabel: "reqID",
+  genReqId() {
+    return uuidv4();
+  },
+  logger: {
+    stream: pino.multistream(streams),
+    level: "info",
+  },
+  disableRequestLogging: true,
+});
+await fastify.register(compress, { encodings: ["gzip"] });
 //fastify metrics
-await fastify.register(metricsPlugin, { endpoint: '/metrics' });
+await fastify.register(metricsPlugin, { endpoint: "/metrics" });
 
 //Logger Plugin
 await fastify.register(loggerPlugin);
- 
+
 // JWT Plugin
-await fastify.register(fastifyJWTPlugin,{ secret : config.jwt.secret})
+await fastify.register(fastifyJWTPlugin, { secret: config.jwt.secret });
 
 // MYSQL Plugin
-await fastify.register(fastifyMysql2Plugin,config.db)
-await fastify.register(fastifyMysql2DmlPlugin,config.db_dml)
+await fastify.register(fastifyMysql2Plugin, config.db);
+await fastify.register(fastifyMysql2DmlPlugin, config.db_dml);
 
 // Format Date Plugin
-await fastify.register(fastifyTanggalIndoPlugin)
+await fastify.register(fastifyTanggalIndoPlugin);
 // Mqtt Plugin
-await fastify.register(mqttPlugin)
+await fastify.register(mqttPlugin);
 
 // REDIS Plugin
-await fastify.register(redis, config.redis)
+await fastify.register(redis, config.redis);
 
 // CORS Plugin
-await fastify.register(cors, { 
-    origin: '*',
-    methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
-    credentials:true
-})  
+await fastify.register(cors, {
+  origin: "*",
+  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+  credentials: true,
+});
 
 // await fastify.register(fastifyStatic,{
 //   root: join(__dirname, 'statics')
 // })
-await fastify.register(fastifyStatic,{
-  root: join(__dirname, 'public'),
-})
+await fastify.register(fastifyStatic, {
+  root: join(__dirname, "public"),
+});
 // Register Controller (Routing akses untuk client)
 await fastify.register(autoLoad, {
-  dir: join(__dirname, 'controllers'),
-  options: { 
-      prefix: '/'
+  dir: join(__dirname, "controllers"),
+  options: {
+    prefix: "/",
   },
-})
+});
+
+let taskRunning = true;
+let taskRunningUrgent = true;
+let taskRunningIn = true;
+cron.schedule("15,27,45,59 * * * *", async () => {
+  if (taskRunning) {
+    try {
+      taskRunning = false;
+      console.log("Call User");
+      const promise = [
+        getData(fastify, "req", "RE01", "VOICE_USER_REG1.mp3"),
+        getData(fastify, "req", "RE02", "VOICE_USER_REG2.mp3"),
+        getData(fastify, "req", "RE03", "VOICE_USER_REG3.mp3"),
+        getData(fastify, "req", "RE04", "VOICE_USER_REG4.mp3"),
+      ];
+      await Promise.allSettled(promise);
+      taskRunning = true;
+    } catch (err) {
+      taskRunning = true;
+      console.log("ERROR" + err);
+    }
+  }
+});
+
+cron.schedule("13,23,33,43,53 * * * *", async () => {
+  if (taskRunningIn) {
+    try {
+      taskRunningIn = false;
+      console.log("Call Inprogress");
+      const promise = [
+        inprogress.getData(fastify, "req", "RE01", "VOICE_INPROGRESS_REG1.mp3"),
+        inprogress.getData(fastify, "req", "RE02", "VOICE_INPROGRESS_REG2.mp3"),
+        inprogress.getData(fastify, "req", "RE03", "VOICE_INPROGRESS_REG3.mp3"),
+        inprogress.getData(fastify, "req", "RE04", "VOICE_INPROGRESS_REG4.mp3"),
+      ];
+      await Promise.allSettled(promise);
+      taskRunningIn = true;
+    } catch (err) {
+      taskRunningIn = true;
+      console.log("ERROR" + err);
+    }
+  }
+});
+
+cron.schedule("*/4 * * * *", async () => {
+  if (taskRunningUrgent) {
+    try {
+      taskRunningUrgent = false;
+      console.log("Call Urgent");
+      const promise = [
+        urgent.getData(fastify, "req", "RE01", "VOICE_URGENT_REG1.mp3"),
+        urgent.getData(fastify, "req", "RE02", "VOICE_URGENT_REG2.mp3"),
+        urgent.getData(fastify, "req", "RE03", "VOICE_URGENT_REG3.mp3"),
+        urgent.getData(fastify, "req", "RE04", "VOICE_URGENT_REG4.mp3"),
+      ];
+      await Promise.allSettled(promise);
+      taskRunningUrgent = true;
+    } catch (err) {
+      taskRunningUrgent = true;
+      console.log("ERROR" + err);
+    }
+  }
+});
--- services/urgent/index.js
+++ services/urgent/index.js
@@ -1,49 +1,59 @@
 import gTTS from "gtts";
-import path from 'path';
+import path from "path";
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export const getData =  async(fastify,req,region,filename)=>{
-    try { 
-        
-        const { alerts } = req.body
-        
-        if(alerts[0].labels.toko.length === 0) return {code: 200, message: "Tidak Ada Data"}
+export const getData = async (fastify, req, region, filename) => {
+  try {
+    //const { alerts } = req.body
 
-        const textSuara = `Perhatian... Perhatian... Ada ${alerts[0].labels.toko.split(",").length} komplen bersifat urgent, toko ${alerts[0].labels.toko}. Tolong segera di cek... terima kasih.`
-        const outputPath = path.join(process.cwd(), 'public', filename);
+    const conn = fastify.mysql;
+    const [alerts] = await conn.query(`select count(*) as Total_CO,group_concat(kode_lokasi_asal) as toko 
+        from dt_complain where 
+        Kode_Lokasi_Tujuan ='${region}'
+        and (Deskripsi rlike 'pesan|klik|apka|grab|istore|dms|sales|kasir|jual|absen|kasir|jual|pos|posmain' or masalah rlike'KLIK')
+        and Status_Komplain rlike 'OPEN'
+        order by Total_CO`);
 
-        var gtts = new gTTS(textSuara, 'id');
-        gtts.save(outputPath, function (err, result) {
-          if(err) { throw new Error(err) }
-          console.log(`Success! Open file ${outputPath} to hear result.`);
-        });
-        await sleep(3000)
-        fastify.mqttClient.publish(`start-urgent-${region.toLowerCase().replace(/0/g, 'g')}`,"1", (err) => {
-            if (err) {
-                console.error('Error publishing message:', err);
-                return { 
-                    code: 500,
-                    message: err
-                } 
-            } else {
-              console.log(`Published message 1 to topic "start-urgent-${region.toLowerCase().replace(/0/g, 'g')}"`);
-            }
-        });
-        return { 
-            code: 200,
-            message: "Sukses",
-            data: textSuara
-        } 
+    if (alerts[0].Total_CO === 0) return { code: 200, message: "Tidak Ada Data" };
 
-    } catch (error) {
-        fastify.log.error(error, "Error Service (/services/urgent/index.js)")
-        return { 
-            code: 500,
-            message: "Internal Server Error",
-            error: error.message
-        }
-    }
-}
+    const textSuara = `Perhatian... Perhatian... Ada ${
+      alerts[0].toko.split(",").length
+    } komplen bersifat urgent, toko ${alerts[0].toko}. Tolong segera di cek... terima kasih.`;
+    const outputPath = path.join(process.cwd(), "public", filename);
+
+    var gtts = new gTTS(textSuara, "id");
+    gtts.save(outputPath, function (err, result) {
+      if (err) {
+        throw new Error(err);
+      }
+      console.log(`Success! Open file ${outputPath} to hear result.`);
+    });
+    await sleep(3000);
+    fastify.mqttClient.publish(`start-urgent-${region.toLowerCase().replace(/0/g, "g")}`, "1", (err) => {
+      if (err) {
+        console.error("Error publishing message:", err);
+        return {
+          code: 500,
+          message: err,
+        };
+      } else {
+        console.log(`Published message 1 to topic "start-urgent-${region.toLowerCase().replace(/0/g, "g")}"`);
+      }
+    });
+    return {
+      code: 200,
+      message: "Sukses",
+      data: textSuara,
+    };
+  } catch (error) {
+    fastify.log.error(error, "Error Service (/services/urgent/index.js)");
+    return {
+      code: 500,
+      message: "Internal Server Error",
+      error: error.message,
+    };
+  }
+};
